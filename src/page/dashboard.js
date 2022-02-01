ce48c613cf06b08da4323934bcef73409aabaf6a
shantanukulkarni27 <shantanu.kulkarni@successive.tech>
2022-02-01 16:42:51 +0530

    get api working

--- src/page/dashboard.js
+++ src/page/dashboard.js
@@ -1,16 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DashHead } from "../components/styled";
 import ThemeButton from "../components/themeButton";
-import ThemeContext,{themes} from "../contexts/themeContext";
+import ThemeContext, { themes } from "../contexts/themeContext";
 import { Header } from "../module/index";
-const Dashboard = () =>{
+import axios from 'axios';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import EmpModal from "../components/EmpModal";
+import DisplayTable from "./DisplayTable";
+
+const Dashboard = () => {
+  
     const themes = useContext(ThemeContext)
-    return(
+
+  
+    // let a=[];
+    // result.forEach((obj)=>{
+    //     a.push(obj.title)
+    // })
+    // console.log("res is ", result)
+
+    // console.log(a)
+    return (
         <>
-        <div style={themes.theme}>           
-        <Header/>
-        <DashHead>Welcome to dashboards</DashHead>
-        </div>    
+            <div style={themes.theme}>
+                <Header />
+                <DashHead>Welcome to dashboards</DashHead>
+                <DisplayTable/>
+           
+
+         
+            {/* <EmpModal/> */}
+
+            </div>
         </>
     )
 }
